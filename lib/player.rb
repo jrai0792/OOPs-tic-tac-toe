83962ada571f5553989732f1996341a286c9afcb
Jyoti <jyotikiran12@gmail.com>
2020-09-23 13:21:06 +0530

    Fixed linters errors.

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,8 @@
 class Player
-    attr_accessor :name, :key, :array
-    def initialize(name, key)
-      @name = name
-      @key = key
-      @array = []
-    end
+  attr_accessor :name, :key, :array
+  def initialize(name, key)
+    @name = name
+    @key = key
+    @array = []
   end
+end
